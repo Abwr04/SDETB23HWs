dbead19c25fe1c5aa4fdc01c125a4e038a0b742a
Abwr04 <awabanwar4.444@gmail.com>
2025-04-16 23:40:49 -0400

    Update E2HelloWorld.java

--- src/main/java/org/example/week1/d3/e2/E2HelloWorld.java
+++ src/main/java/org/example/week1/d3/e2/E2HelloWorld.java
@@ -3,6 +3,7 @@ package org.example.week1.d3.e2;
 public class E2HelloWorld {
     public static void main(String[] args) {
 
-        //Write code here to Print
+       System.out.println("Hello, World!");
+       System.out.println("Welcome to Java!");
     }
 }
